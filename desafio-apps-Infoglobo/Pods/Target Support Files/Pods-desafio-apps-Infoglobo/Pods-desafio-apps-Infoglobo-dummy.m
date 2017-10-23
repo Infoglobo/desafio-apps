0dadfbaf5d172cb233747b7c71b5d1f9703f05a9
Renan Matias <rnmatias@hotmail.com>
2017-10-22 22:19:50 -0200

    Gerenciado-Dependencias
    CocoaPods:
    ObjectMapper
    PINRemoteImage

---
+++ desafio-apps-Infoglobo/Pods/Target Support Files/Pods-desafio-apps-Infoglobo/Pods-desafio-apps-Infoglobo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_desafio_apps_Infoglobo : NSObject
+@end
+@implementation PodsDummy_Pods_desafio_apps_Infoglobo
+@end
---
+++ desafio-apps-Infoglobo/Pods/Target Support Files/Pods-desafio-apps-InfogloboTests/Pods-desafio-apps-InfogloboTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_desafio_apps_InfogloboTests : NSObject
+@end
+@implementation PodsDummy_Pods_desafio_apps_InfogloboTests
+@end
