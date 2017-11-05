a9a115a6561305d31e530e37d3811630d807ed0e
Andre Nogueira <avinogueira@yahoo.com>
2017-11-05 10:01:04 -0200

    Ajustando constraints

--- globoapp/controllers/CapaViewController.swift
+++ globoapp/controllers/CapaViewController.swift
@@ -12,8 +12,7 @@ import Foundation
 
 class CapaViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     var noticiaAPICall: NoticiasAPICall!
-    //    var noticiaViewModel : NoticiaViewModel!
-    
+
     @IBOutlet weak var capaView: UIView!
     
     @IBOutlet weak var tableView: UITableView!
@@ -134,6 +133,13 @@ class CapaViewController: UIViewController, UITableViewDataSource, UITableViewDe
             noticiaVC.setUpNoticiaView(titulo: new.title!, subtitulo: new.subTitle!, imagem: new.images[0].image! as Data, texto: new.texto!)
         }
     
+    func setUpCapa(new: Noticia, secao: Secao){
+        //        self.tableView.setup(secao: (secao.name)!, titulo: (new.title)!)
+        //        self.capaTableView.imagemCapa.image = UIImage(data: new.images[0].image! as Data)
+    }
+    
+}
+
 //    func setUpCellImage(cell: NoticiaTableViewCell, new: Noticia){
 //        //        cell.imagemNoticia.image = UIImage(data: new.images[0].image! as Data)
 //        let itemSize:CGSize = CGSize(width: 99, height: 90)
@@ -152,47 +158,30 @@ class CapaViewController: UIViewController, UITableViewDataSource, UITableViewDe
 //            return ;
 //        }
 //    }
-    
-    func setUpCapa(new: Noticia, secao: Secao){
-        //        self.tableView.setup(secao: (secao.name)!, titulo: (new.title)!)
-        //        self.capaTableView.imagemCapa.image = UIImage(data: new.images[0].image! as Data)
-    }
-    
-   
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
-        let size = image.size
-        
-        let widthRatio  = targetSize.width  / size.width
-        let heightRatio = targetSize.height / size.height
-        
-        // Figure out what our orientation is, and use that to form the rectangle
-        var newSize: CGSize
-        if(widthRatio > heightRatio) {
-            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
-        } else {
-            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
-        }
-        
-        // This is the rect that we've calculated out and this is what is actually used below
-        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
-        
-        // Actually do the resizing to the rect using the ImageContext stuff
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
-        image.draw(in: rect)
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return newImage!
-    }
-    
-}
+
+//    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+//        let size = image.size
+//
+//        let widthRatio  = targetSize.width  / size.width
+//        let heightRatio = targetSize.height / size.height
+//
+//        // Figure out what our orientation is, and use that to form the rectangle
+//        var newSize: CGSize
+//        if(widthRatio > heightRatio) {
+//            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+//        } else {
+//            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+//        }
+//
+//        // This is the rect that we've calculated out and this is what is actually used below
+//        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+//
+//        // Actually do the resizing to the rect using the ImageContext stuff
+//        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+//        image.draw(in: rect)
+//        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//
+//        return newImage!
+//    }
 
--- globoapp/controllers/NoticiaViewController.swift
+++ globoapp/controllers/NoticiaViewController.swift
@@ -14,26 +14,14 @@ class NoticiaViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     public func setUpNoticiaView(titulo: String, subtitulo: String, imagem: Data, texto : String){
         self.noticiaView.setUpNoticiaView(titulo: titulo, subtitulo: subtitulo, imagem: imagem, texto: texto)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
