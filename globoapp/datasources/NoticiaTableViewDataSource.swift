da39663ef671d56907364b561a849939f205d9c7
Andre Nogueira <avinogueira@yahoo.com>
2017-11-05 23:46:15 -0200

    Deleting files

--- globoapp/datasources/NoticiaTableViewDataSource.swift
+++ globoapp/datasources/NoticiaTableViewDataSource.swift
@@ -8,66 +8,8 @@
 //
 //import UIKit
 //
-//class NoticiaTableViewDataSource: UITableViewDataSource {
-//    func `self`() -> Self {
-//        return self
-//    }
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 1
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        return UITableViewCell()
-//    }
-//
-//    func isEqual(_ object: Any?) -> Bool {
-//
-//    }
-//
-//    var hash: Int = 0
-//
-//    var superclass: AnyClass?
-//
-//    func perform(_ aSelector: Selector!) -> Unmanaged<AnyObject>! {
-//        return nil
-//    }
-//
-//    func perform(_ aSelector: Selector!, with object: Any!) -> Unmanaged<AnyObject>! {
-//        return nil
-//    }
-//
-//    func perform(_ aSelector: Selector!, with object1: Any!, with object2: Any!) -> Unmanaged<AnyObject>! {
-//        return nil
-//    }
-//
-//    func isProxy() -> Bool {
-//        return false
-//
-//    }
-//
-//    func isKind(of aClass: AnyClass) -> Bool {
-//        return false
-//
-//    }
-//
-//    func isMember(of aClass: AnyClass) -> Bool {
-//        return false
-//
-//    }
-//
-//    func conforms(to aProtocol: Protocol) -> Bool {
-//        return false
-//
-//    }
-//
-//    func responds(to aSelector: Selector!) -> Bool {
-//        return false
-//    }
-//
-//    var description: String = ""
-//
+////class NoticiaTableViewDataSource: UITableViewDataSource {
 //
 //
 //}
-
+//
