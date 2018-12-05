e62dc7fc96eaecc559c3b45a2454d9644aa70cff
Alan Camargo <alan.uk.br@gmail.com>
2018-12-05 16:52:25 -0200

    Implemented cache to API responses

--- app/src/main/kotlin/com/alancamargo/desafioinfoglobo/api/ProductApi.kt
+++ app/src/main/kotlin/com/alancamargo/desafioinfoglobo/api/ProductApi.kt
@@ -1,7 +1,10 @@
 package com.alancamargo.desafioinfoglobo.api
 
+import android.content.Context
+import android.net.ConnectivityManager
 import com.alancamargo.desafioinfoglobo.model.Product
-import com.google.gson.GsonBuilder
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -9,18 +12,47 @@ import retrofit2.http.GET
 
 interface ProductApi {
 
-    @GET("")
+    @GET("Infoglobo/desafio-apps/master/capa.json")
     fun getProducts(): Call<List<Product>>
 
     companion object {
-        fun getApi(baseUrl: String): ProductApi {
-            val gson = GsonBuilder().setLenient().create()
-
+        fun getApi(baseUrl: String, context: Context): ProductApi {
             return Retrofit.Builder()
                 .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(createHttpClient(context))
                 .build()
                 .create(ProductApi::class.java)
+        }
+
+        private fun createHttpClient(context: Context): OkHttpClient {
+            val cacheSize = (5 * 1024 * 1024).toLong()
+            val myCache = Cache(context.cacheDir, cacheSize)
+
+            return OkHttpClient.Builder()
+                .cache(myCache)
+                .addInterceptor { chain ->
+                    var request = chain.request()
+
+                    // If there is network, look for the response cached up to 5 seconds ago
+                    val maxAge = 5
+
+                    // If the cache is older than 7 days, delete it
+                    val maxStale = 60 * 60 * 24 * 7
+
+                    request = if (hasNetwork(context))
+                        request.newBuilder().header("Cache-Control", "public, max-age=$maxAge").build()
+                    else
+                        request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=$maxStale").build()
+                    chain.proceed(request)
+                }
+                .build()
+        }
+
+        private fun hasNetwork(context: Context): Boolean {
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetwork = connectivityManager.activeNetworkInfo
+            return activeNetwork.isConnected
         }
     }
 
