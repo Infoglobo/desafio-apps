2e8ad5a92bae9f53e631e467c70a0c0433e3b5c6
Alan Camargo <alan.uk.br@gmail.com>
2018-12-06 21:14:12 -0200

    Created navigation to ArticleDetailsActivity

--- app/src/main/kotlin/com/alancamargo/desafioinfoglobo/activities/ArticleDetailsActivity.kt
+++ app/src/main/kotlin/com/alancamargo/desafioinfoglobo/activities/ArticleDetailsActivity.kt
@@ -1,8 +1,11 @@
 package com.alancamargo.desafioinfoglobo.activities
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.alancamargo.desafioinfoglobo.R
+import com.alancamargo.desafioinfoglobo.model.Article
 
 class ArticleDetailsActivity : AppCompatActivity() {
 
@@ -10,6 +13,15 @@ class ArticleDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_details)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    companion object {
+        private const val EXTRA_ARTICLE = "com.alancamargo.desafioinfoglobo.EXTRA_ARTICLE"
+
+        fun getIntent(context: Context, article: Article): Intent {
+            return Intent(context, ArticleDetailsActivity::class.java)
+                .putExtra(EXTRA_ARTICLE, article)
+        }
     }
 
 }
