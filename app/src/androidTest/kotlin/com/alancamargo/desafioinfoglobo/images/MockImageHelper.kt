b80a9363452153627067093da55df71ee6292161
Alan Camargo <alan.uk.br@gmail.com>
2018-12-08 21:41:52 -0200

    Fixed layouts

--- app/src/androidTest/kotlin/com/alancamargo/desafioinfoglobo/images/MockImageHelper.kt
+++ app/src/androidTest/kotlin/com/alancamargo/desafioinfoglobo/images/MockImageHelper.kt
@@ -1,11 +1,12 @@
 package com.alancamargo.desafioinfoglobo.images
 
 import android.widget.ImageView
+import com.alancamargo.desafioinfoglobo.R
 
 object MockImageHelper : ImageHelper {
 
     override fun loadImage(imageUrl: String, target: ImageView) {
-        target.setImageResource(android.R.drawable.ic_menu_gallery)
+        target.setImageResource(R.drawable.cabral_preso)
     }
 
 }
